6fbf381e64cf975d27cede030e3ac2fa5957b90b
AdrianoJ17 <167003164+AdrianoJ17@users.noreply.github.com>
2024-05-19 17:45:38 +0200

    Add files via upload

--- test/it/uniroma3/diadia/PartitaTest.java
+++ test/it/uniroma3/diadia/PartitaTest.java
@@ -1,6 +1,8 @@
 package it.uniroma3.diadia;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
